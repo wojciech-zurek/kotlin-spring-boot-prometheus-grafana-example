c4384e783a4079bcaafae3704b5c6b27723a37e0
Wojciech Zurek <mail@wojciechzurek.eu>
2018-03-06 20:10:03 +0100

    travis support

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-./gradlew clean assemble
+./gradlew clean check assemble
 docker build -t eu.wojciechzurek/custom-prometheus:0.0.1 -t eu.wojciechzurek/custom-prometheus:latest -f docker/prometheus/Dockerfile .
 docker build -t eu.wojciechzurek/spring-boot-example-metric:0.0.1 -t eu.wojciechzurek/spring-boot-example-metric:latest -f docker/app/Dockerfile .
